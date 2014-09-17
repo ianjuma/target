b1f2b88f6a008e1b60955b20f5a897c118b7d011
Ian Juma <ian.juma@yahoo.com>
2014-09-17 17:34:18 +0300

    withdraw

--- app/distributors/receive_from_client.py
+++ app/distributors/receive_from_client.py
@@ -9,40 +9,46 @@ from app import g
 from app import logging
 from app import red
 from app import RqlError
-from app import session
+from flask import request
 
 
 from app import welcomeMessage
 from app import send_notification_task
 
 
-@app.route('/api/getTasks/', methods=['POST', 'GET'])
+#from: The number that sent the message
+#to: The number to which the message was sent
+#text: The message content
+#date: The date and time when the message was received
+#id: The internal ID that we use to store this message
+#linkId - ?
+
+@app.route('/oauthCallBack/', methods=['POST'])
 def getTasks():
-    if not request.json:
-        abort(400)
 
-    if request.headers['Content-Type'] != 'application/json; charset=UTF-8':
-        abort(400)
+    if request.method != 'POST':
+        if not request.json:
+            abort(400)
 
-    username = request.json.get('username')
+        if request.headers['Content-Type'] != 'application/json; charset=UTF-8':
+            abort(400)
 
-    taskData = []
-    try:
-        tasks = r.table('Tasks').filter({"username": username}).run(g.rdb_conn)
-        for data in tasks:
-            taskData.append(data)
+        text = request.json.get('text')
 
-    except RqlError:
-        logging.warning('DB code verify failed on /api/getTasks/')
+        try:
+            tasks = r.table('Tasks').filter({"username": username}).run(g.rdb_conn)
+            for data in tasks:
+                taskData.append(data)
 
-        resp = make_response(jsonify({"Error": "503 DB error"}), 503)
+        except RqlError:
+            logging.warning('DB code verify failed on /api/getTasks/')
+
+            resp = make_response(jsonify({"Error": "503 DB error"}), 503)
+            resp.headers['Content-Type'] = "application/json"
+            resp.cache_control.no_cache = True
+            return resp
+
+        resp = make_response(taskData, 200)
         resp.headers['Content-Type'] = "application/json"
         resp.cache_control.no_cache = True
         return resp
-
-    taskData = dumps(taskData)
-
-    resp = make_response(taskData, 200)
-    resp.headers['Content-Type'] = "application/json"
-    resp.cache_control.no_cache = True
-    return resp
--- app/distributors/withdraw_from_bank.py
+++ app/distributors/withdraw_from_bank.py
@@ -7,7 +7,6 @@ from app import app
 from app import r
 from app import g
 from app import logging
-from app import salt
 from app import red
 from app import RqlError
 
@@ -16,36 +15,12 @@ from app import welcomeMessage
 from app import send_notification_task
 
 
-@app.route('/admin/', methods=['POST', 'GET'])
-def adminSign():
-    if request.method == 'POST':
+def withdraw(username):
+    try:
+        user = r.table('Admin').get(username).run(g.rdb_conn)
+    except Exception, e:
+        logging.warning('DB failed on /admin/ -> user not found')
+        raise e
 
-        if not request.json:
-            abort(400)
-
-        if request.headers['Content-Type'] != 'application/json; charset=UTF-8':
-            abort(400)
-
-        username = request.json.get('username')
-        password = request.json.get('password')
-
-        try:
-            user = r.table('Admin').get(username).run(g.rdb_conn)
-        except Exception, e:
-            logging.warning('DB failed on /admin/ -> user not found')
-            raise e
-
-        if user is None:
-            resp = make_response(jsonify({"Not Found": "User Not Found"}), 404)
-            resp.headers['Content-Type'] = "application/json"
-            resp.cache_control.no_cache = True
-            return resp
-
-        session[username] = username
-
-        resp = make_response(jsonify({"OK": "Signed In"}), 200)
-        resp.headers['Content-Type'] = "application/json"
-        resp.cache_control.no_cache = True
-        return resp
-
-    return render_template('adminSignin.html')
+    if user is None:
+        pass
