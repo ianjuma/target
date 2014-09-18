7b4b24b1c3d7afa2884ed61e30d77594ff3c5b30
Ian Juma <ian.juma@yahoo.com>
2014-09-19 00:19:12 +0300

    receive from client

--- app/distributors/receive_from_client.py
+++ app/distributors/receive_from_client.py
@@ -47,3 +47,64 @@ def getTasks():
 
 def receivePayment():
     pass
+
+
+@app.route('/api/getUSSD/', methods=['POST'])
+def ussdCallBack():
+    if request.method is 'POST':
+        if request.headers['Content-Type'] != 'text/plain':
+            abort(400)
+
+        text = request.data
+        sender = request.args.get('from')
+
+        if request.args.get('text') is '':
+            # load menu            
+            menu_text = """Enter menu number to \n
+            1. To pay a distributor \n
+            2. To check balance \n
+            3. To make a credit request \n
+            """
+
+            resp = make_response(menu_text, 200)
+            resp.headers['Content-Type'] = "text/plain"
+            resp.cache_control.no_cache = True
+            return resp
+
+        elif request.args.get('text') is '1':
+            resp = make_response("OK", 200)
+            resp.headers['Content-Type'] = "text/plain"
+            resp.cache_control.no_cache = True
+            return resp
+
+        elif request.args.get('text') is '2':
+            resp = make_response("OK", 200)
+            resp.headers['Content-Type'] = "text/plain"
+            resp.cache_control.no_cache = True
+            return resp
+
+        elif request.args.get('text') is '2':
+            resp = make_response("OK", 200)
+            resp.headers['Content-Type'] = "text/plain"
+            resp.cache_control.no_cache = True
+            return resp
+        else:
+            resp = make_response("OK", 200)
+            resp.headers['Content-Type'] = "text/plain"
+            resp.cache_control.no_cache = True
+            return resp
+
+        try:
+            tasks = r.table('Client').get(sender).update(text).run(g.rdb_conn)
+        except RqlError:
+            logging.warning('DB code verify failed on /api/getTasks/')
+
+            resp = make_response(jsonify({"Error": "503 DB error"}), 503)
+            resp.headers['Content-Type'] = "application/json"
+            resp.cache_control.no_cache = True
+            return resp
+
+        resp = make_response(tasks, 200)
+        resp.headers['Content-Type'] = "application/json"
+        resp.cache_control.no_cache = True
+        return resp
